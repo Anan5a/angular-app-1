932fedd9c2804ac8d291ece01f18b9d9005d8ac6
Sayem Prodhan <ananta.ap69@gmail.com>
2024-07-08 18:14:30 +0600

    till video 50

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,8 +1,18 @@
-import { Component,computed,signal } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
+import { Component, Input, Output, EventEmitter, output, input } from '@angular/core';
 
+//can define types
+type UserType = {
+  id: string,
+  name: string,
+  avatar: string
+}
+//also a way to define types
+interface IUser {
+  id: string,
+  name: string,
+  avatar: string
+}
 
-const randomIndex = () => Math.floor(Math.random() * DUMMY_USERS.length)
 
 
 
@@ -14,16 +24,24 @@ const randomIndex = () => Math.floor(Math.random() * DUMMY_USERS.length)
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex()])
-  //when using signals, should use computed() function
-  imageUrl = computed(() => 'assets/users/' + this.selectedUser().avatar)
-  // get imageUrl() {
-  //   return 'assets/users/' + this.selectedUser().avatar
-  // }
+  //normal, input decorator
+  @Input({ required: true }) user!: {
+    id: string,
+    name: string,
+    avatar: string
+  };
+  @Output() select = new EventEmitter<string>(); //output decorator
+  // select = output<string>() //same as above, but shorter and new
+  //using signals 
+  // avatar = input.required<string>()
+  // name = input.required<string>()
+  // imagePath = computed(() => "assets/users/" + this.avatar())
 
+
+  get imagePath() {
+    return "assets/users/" + this.user.avatar
+  }
   onSelectUser() {
-    console.log("Clicked button")
-    this.selectedUser.set(DUMMY_USERS[randomIndex()])
-
+    this.select.emit(this.user.id)
   }
 }
